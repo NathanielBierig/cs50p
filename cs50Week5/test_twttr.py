60a07cbaa0c38fa5df3937e47d3c6e2d3102d715
Nathaniel Bierig <netanelbie@gmail.com>
2026-07-13 21:06:43 +0300

    Testing the twttr program from week 2 with pytest

---
+++ cs50Week5/test_twttr.py
@@ -0,0 +1,28 @@
+import twttr
+import string
+import random
+VOWELS = ["a","A", "e","E", "i", "I", "o", "O", "u", "U"]
+def generate_random_string():
+    characters = string.ascii_letters + string.digits
+    
+    # Randomly choose characters and merge them
+    return ''.join(random.choices(characters, k=30))
+
+# Example usage
+
+def test_shorten():
+    try:
+        # hard coded (for project)
+        assert twttr.shorten("Hello World") == "Hll Wrld"
+        assert twttr.shorten("123abc") == "123bc"
+        assert twttr.shorten("The blue parrot thoughtfully juggled several avocados while contemplating the meaning of time.") == "Th bl prrt thghtflly jggld svrl vcds whl cntmpltng th mnng f tm."
+# general, optimal
+        for x in range(10):
+            result = twttr.shorten(generate_random_string())
+            for vowel in VOWELS:
+                assert vowel not in result
+    
+    except AssertionError:
+        print("Fail)")
+def main():
+    generate_random_string()
